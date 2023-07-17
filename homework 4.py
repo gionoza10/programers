6c4cf593f4e6cad09c2124e92a8db452576b814c
gionoza10 <138209252+gionoza10@users.noreply.github.com>
2023-07-17 23:17:16 +0400

    Add files via upload

---
+++ homework 4.py
@@ -0,0 +1,9 @@
+# user_name = input ("whats your name: gio ")
+
+# surname = input ("whats your surname: nozadze ")
+
+# age = input ("whats your age: 13 ")
+
+print("how old are you: 13 ")
+print("what time do you want to travel in time: 5years")
+print("how oll will you be when you comeback from time travel: 18")
